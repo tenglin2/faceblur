c2a69ca1bc8586bec664f13a4d7e2f9600de995c
Teng Lin <tenglin2@gmail.com>
2019-08-09 20:07:58 -0400

    basic structuring

--- src/components/App.js
+++ src/components/App.js
@@ -35,23 +35,39 @@
 // export default App;
 
 import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { changeName } from '../actions/actions';
 import { HashRouter as Router, Route } from 'react-router-dom';
 import Navbar from './Navbar';
 import Home from './Home';
+import About from './About';
+import Register from './Register';
+import Login from './Login';
 import Footer from './Footer';
 
+import '../stylesheets/App.css';
+
 const App = function() {
+	// So here we get a reference to the dispatch function which accepts an action creator. THen we invoke it with the changeName action. This should return an action with a type a payload to the action reducer (and all other reducers). That should update state. And it works. Good!
+	// It seems that this step is necessary for some reason. Probably something to do with asynchronous actions.
+	// For now we don't need any logic. Just do the front end stuff. Reference the
+	const dispatch = useDispatch();
+	dispatch(changeName('gordon'));
+	const name = useSelector((state) => state.name);
+	console.log(name);
+
 	return (
 		<Router>
 			<div class="App">
-				<h1>App Component</h1>
+				{/* <h1>App Component</h1> */}
 
 				<Navbar />
-				<Route exact={true} path="/" component={Home} />
-				<Route exact={true} path="/" component={Home} />
-				<Route exact={true} path="/" component={Home} />
-				<Route exact={true} path="/" component={Home} />
-				<Route exact={true} path="/" component={Home} />
+				{/* Or operator here to make no change and Home have the same component render. */}
+				<Route exact={true} path="/(|Home)" component={Home} />
+				<Route exact={true} path="/About" component={About} />
+				{/* Register and SignIn need to be protected based off of logged in state. */}
+				<Route exact={true} path="/Register" component={Register} />
+				<Route exact={true} path="/Login" component={Login} />
 				<Footer />
 			</div>
 		</Router>
