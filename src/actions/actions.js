4218cdc42551ed3c4c06236580266f590149e3d6
Teng Lin <tenglin2@gmail.com>
2019-08-10 12:52:59 -0400

    added actions for input and registers stuff

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -19,3 +19,43 @@ export const changeName = function(name) {
 		payload: name
 	};
 };
+
+export const handleInputChange = function(input) {
+	console.log(input);
+
+	return {
+		type: 'INPUT_CHANGE',
+		payload: input
+	};
+};
+
+export const registerNameChange = function(name) {
+	console.log('name is', name);
+	return {
+		type: 'REGISTER_NAME_CHANGE',
+		payload: name
+	};
+};
+
+export const registerEmailChange = function(email) {
+	console.log('emial is', email);
+	return {
+		type: 'REGISTER_EMAIL_CHANGE',
+		payload: email
+	};
+};
+
+export const registerPasswordChange = function(password) {
+	console.log('password is', password);
+	return {
+		type: 'REGISTER_PASSWORD_CHANGE',
+		payload: password
+	};
+};
+
+// export const clearRegisterQueries = function() {
+// 	// We just need to call the other actions with empty input...
+// 	registerNameChange('');
+// 	registerEmailChange('');
+// 	registerPasswordChange('');
+// };
